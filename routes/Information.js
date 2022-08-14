a5a8f28c0f6a2ad5fe100dc8e0075c24454159ee
harsh <harshkhosla9945@gmail.com>
2022-08-14 19:22:26 +0530

    harsh updates

--- routes/Information.js
+++ routes/Information.js
@@ -1,13 +1,54 @@
 const express = require('express');
 const router = express.Router();
 var fetchUser= require('../middleware/fetchuser')
-const infornation =require("../modules/Information")
+const information =require("../modules/Information")
 const { body, validationResult } = require('express-validator');
+
+// router.post('/information/:id',[fetchUser],async(req,res)=>{
+//     try{
+//         const errors = validationResult(req);
+//         let user =await information.findOne({email:req.body.email});
+//   if (user){
+//       return res.status(400).json({error:"Sorry user already exists "})
+//   }
+//   const {email,name,Adress,adharNo,PhoneNo}=req.body
+//   const newInformation={}
+//   if(name){newInformation.name=name}
+//   if(email){newInformation.email=email} 
+//   let information =await information.findById(req.params.id);
+
+//   if(!information){information =new information({
+//     name,email,Adress,adharNo,PhoneNo,user:req.user.id
+//     // -------------------------------------- addressLine1:Adress[0].addressLine1--------------------------------------------------//
+// })
+// const SaveInformation = await information.save()
+// res.json(SaveInformation)
+// }
+
+//     if (information.user!=req.user.id){
+//         return res.status(401).send('Hacker')
+//     } information = await information.findByIdAndUpdate(req.params.id,{$set:newInformation},{new:true})
+
+
+         
+//         res.json(information)
+       
+//     }catch(error){
+//         // console.error(error.message);
+//         res.status(500).send("Somthing went wrong")
+//     }
+ 
+ 
+// })
+
+
+
+// ----------------------------------------------------------need to check this------------------------------------------------------------------//
 
 router.post('/information',[fetchUser],async(req,res)=>{
     try{
         const errors = validationResult(req);
-        let user =await infornation.findOne({email:req.body.email});
+        let user =await information.findOne({email:req.body.email});
   if (user){
       return res.status(400).json({error:"Sorry user already exists "})
   }
@@ -15,7 +56,7 @@ router.post('/information',[fetchUser],async(req,res)=>{
    
         const name =req.body.name
         const {email,Adress,adharNo,PhoneNo}=req.body
-        const information =new  infornation({
+        const information =new  information({
             name,email,Adress,adharNo,PhoneNo,user:req.user.id
             // -------------------------------------- addressLine1:Adress[0].addressLine1--------------------------------------------------//
         })
@@ -29,10 +70,23 @@ router.post('/information',[fetchUser],async(req,res)=>{
  
  
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
 router.get('/fetchallData',fetchUser,async(req,res)=>{
     try{
         
-        const notes =await infornation.find({user:req.user.id})
+        const notes =await information.find({user:req.user.id})
         res.json(notes)
     }catch(error){
         console.error(error.message);
