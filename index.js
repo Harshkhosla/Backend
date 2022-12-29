909352b6b8425db0f9d194abe6918ec8dc7cf9a8
harsh <harshkhosla9945@gmail.com>
2022-12-29 16:20:57 +0530

    pussing the changes

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(
 
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
+app.use('/api/Taskep',require('./routes/Taskep'))
 app.use('/api/health',require('./routes/healthId'))
 app.use('/api/Image',require('./routes/Imagedata'))
 app.use('/api/pdf',require('./routes/pdfData'))
