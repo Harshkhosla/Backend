60a86782b6b8cdaf9ddeefff55cfb2d4ee9c4b9c
Harshkhosla <harshkhosla9945@gmail.com>
2024-03-16 12:53:28 +0530

    port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const http = require('http');
 ConnectToMongo();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5200;
 
 app.use(cors());
 app.use(express.json());
